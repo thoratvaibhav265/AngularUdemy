9181b3d7d97b86b94b259a4f82ad7b5ebd09d8b4
Vaibhav <thoratvaibhav265@gmail.com>
2026-09-22 14:31:57 +0530

    manu changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-
+import { User } from './user/user';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.html',  
     standalone: true,
-    imports: [HeaderComponent]
+    styleUrls: ['./app.css'],
+    imports: [HeaderComponent,User]
 })
 export class App{
 
---
+++ src/app/user/user.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+
+}
