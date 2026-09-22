9637f73297690b7b8a90f3d1a922a19715672a11
Vaibhav <thoratvaibhav265@gmail.com>
2026-09-22 15:03:45 +0530

    Name changes with Dummy data

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// './' ka matlab hai current folder (user folder) ke andar dekhna
+import { DUMMY_USERS } from '../dummy_data/dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -8,5 +12,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
-
+  selectedUser=DUMMY_USERS[randomIndex];
 }
