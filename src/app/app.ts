5ec67f1c8e0655f95821d4eda49e79242985c147
Vaibhav <thoratvaibhav265@gmail.com>
2026-09-22 23:25:47 +0530

    changes related to task status

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,27 @@
 
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { User } from './user/user';
+import {  UserComponent } from './user/user';
 import { DUMMY_USERS } from './dummy_data/dummy-users';
+import { Task } from './task/task';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.html',  
     standalone: true,
     styleUrls: ['./app.css'],
-    imports: [HeaderComponent,User]
+    imports: [HeaderComponent, UserComponent, Task]
 })
 export class App{
 
-    users = DUMMY_USERS;
+     users = DUMMY_USERS;
+     selectedUserId: string | null = null;
+
+     get selectedUser(){
+        return this.users.find(user => user.id === this.selectedUserId);
+     }
+     
+     onUserSelected(userId: string) {
+        this.selectedUserId = userId;
+     }
 }
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,8 +1,10 @@
-import { Component, Input,input,computed} from '@angular/core';
+import { Component, Input,input,computed, Output, EventEmitter} from '@angular/core';
 // './' ka matlab hai current folder (user folder) ke andar dekhna
 import { DUMMY_USERS } from '../dummy_data/dummy-users';
 
-
+type User={
+  id: string; name: string; avatar: string 
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -10,19 +12,22 @@ import { DUMMY_USERS } from '../dummy_data/dummy-users';
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
-export class User {
-  //@Input({required: true}) avatar!:string;
+export class UserComponent {
+  @Input({required: true}) user!: User;
 
-  //@Input({required: true}) name!:string;
-  avatar=input<string>;
-  name=input<string>;
- 
-  imagePath=computed(( ) => {
-    return 'assets/images/users/' + this.avatar;
-});
-  // get imagePath(): string {
-  //   return 'assets/images/users/' + this.avatar;
-  // }
+  @Output() select = new EventEmitter<string>();
 
-  onSelectuser() {}
+//   avatar=input<string>;
+//   name=input<string>;
+//   imagePath=computed(( ) => {
+//     return 'assets/images/users/' + this.avatar;
+// });
+
+  get imagePath(): string {
+    return 'assets/images/users/'+ this.user.avatar;
+  }
+
+  onSelectuser() {
+    this.select.emit(this.user.id);
+  }
 }
